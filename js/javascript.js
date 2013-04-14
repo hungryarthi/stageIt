ae92e27146f5e6441faa54a04cfb454452b37b83
Tami Forrester <tforrest@mit.edu>
2013-04-14 13:18:22 -0400

    minor edits to css

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 $('#stages li').click(function(){
-    $(this).addClass('selected').siblings().removeClass('selected');
+    $(this).addClass('stage-selected').siblings().removeClass('stage-selected');
 });
 
 $('#editStage').click(function(){
@@ -9,7 +9,7 @@ $('#editStage').click(function(){
 
 // things that need to be reset when stageModal is hidden
 $('#chooseStage').on('hidden', function(){
-	$('#stages li').removeClass('selected');
+	$('#stages li').removeClass('stage-selected');
 	$('#stageHelper').css("display", "none");
 })
 
@@ -26,13 +26,14 @@ function closeStageDialog() {
 //function called when ok button is clicked on stage modal
 function drawStage() {
 	//check if there is something selected
-	if(!$('.selected').length > 0){
+	if(!$('.stage-selected').length > 0){
 		// alert("")
 		$('#stageHelper').css("display", "inline");
 	}
 	else{
-		// console.log($('.selected').attr('id'));
-		var stage = $('.selected').attr('id');
+		// console.log($('.stage-selected').attr('id'));
+		var stage = $('.stage-selected').attr('id');
+
 		closeStageDialog();
 	}
 	
