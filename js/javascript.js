eaae0add762cc1243acc0578c4aebd5f0b10b416
Tami Forrester <tforrest@mit.edu>
2013-04-14 02:26:38 -0400

    edits to editStage modal form
    added placeholders for image buttons

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,17 @@
+$('#stages li').click(function(){
+    $(this).addClass('selected').siblings().removeClass('selected');
+});
+
 $('#editStage').click(function(){
+	$('#stages li').removeClass('selected');
 	$('#chooseStage').modal();
+
 });
 $('#addDancers').click(function(){
 	$('#addDancersModal').modal();
 });
+
+
 
 (function(){
     
